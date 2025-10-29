b4587797360846204657e3fa9e7da12bb5a33ad1
coldweaver <gerritschirm@googlemail.com>
2025-10-29 13:21:23 +0100

    last commit before inevitably breaking everything

--- ygodatabase-backend/src-tauri/src/lib.rs
+++ ygodatabase-backend/src-tauri/src/lib.rs
@@ -1,14 +1,13 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri_plugin_log::{LogTarget, Builder};
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_log::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(
+            Builder::default()
+                .targets([LogTarget::LogDir, LogTarget::Stdout])
+                .build(),
+        )
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
